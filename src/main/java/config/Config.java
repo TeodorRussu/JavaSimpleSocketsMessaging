c38dc90ba246d83c525781dc5a2ee32f492384b8
trussu <teodor.russu@verizonmedia.com>
2020-05-11 02:11:17 +0300

    update

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -4,7 +4,7 @@ public class Config {
     private Config(){}
 
     public static final String HOST = "localhost";
-    public static final int PORT = 35679;
+    public static final int PORT = 35689;
     public static final int MESSAGES_LIMIT = 10;
     public static final String STOP_PLAYERS_MESSAGE = "No more messages for today, have some rest, you deserve it!";
     public static final String START_MESSAGE = "Hel"
